c9743502957108c8daec91ef504a38ade7312ad5
Minjun Kim <damon911@naver.com>
2023-06-15 01:24:31 +0900

    Part 1 chapter7

--- Part1/chapter7/app/src/main/java/fastcampus/part1/chapter7/MainActivity.kt
+++ Part1/chapter7/app/src/main/java/fastcampus/part1/chapter7/MainActivity.kt
@@ -1,11 +1,132 @@
 package fastcampus.part1.chapter7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.part1.chapter7.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
+
+    private var selectedWord: Word? = null
+
+    private val updateAddWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val isUpdated = result.data?.getBooleanExtra("isUpdated", false) ?: false
+        if (result.resultCode == RESULT_OK && isUpdated) {
+            updateAddWord()
+        }
+    }
+
+    private val updateEditWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val editWord = result.data?.getParcelableExtra<Word>("editWord")
+
+        if (result.resultCode == RESULT_OK && editWord != null) {
+            updateEditWord(editWord)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+
+        binding.btnAdd.setOnClickListener {
+            Intent(this, AddActivity::class.java).let {
+                updateAddWordResult.launch(it)
+            }
+        }
+
+        binding.ivDelete.setOnClickListener {
+            deleteWord()
+        }
+
+        binding.ivEdit.setOnClickListener {
+            editWord()
+        }
+    }
+
+    private fun updateAddWord() {
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
+                wordAdapter.list.add(0, word)
+                runOnUiThread { wordAdapter.notifyDataSetChanged() }
+            }
+        }.start()
+    }
+
+    private fun updateEditWord(word: Word) {
+        val index = wordAdapter.list.indexOfFirst { it.id == word.id }
+        wordAdapter.list[index] = word
+        runOnUiThread {
+            selectedWord = word
+            wordAdapter.notifyItemChanged(index)
+            binding.tvWord.text = word.word
+            binding.tvWordMean.text = word.mean
+        }
+    }
+
+    private fun initRecyclerView() {
+        wordAdapter = WordAdapter(mutableListOf(), this)
+
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+
+        Thread {
+            val list = AppDatabase.getInstance(this)?.wordDao()?.getAll() ?: emptyList()
+            wordAdapter.list.addAll(list)
+            runOnUiThread { wordAdapter.notifyDataSetChanged() }
+        }.start()
+    }
+
+    private fun deleteWord() {
+        if (selectedWord == null)
+            return
+
+        Thread {
+            selectedWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.delete(word)
+                runOnUiThread {
+                    binding.tvWord.text = ""
+                    binding.tvWordMean.text = ""
+
+                    wordAdapter.list.remove(word)
+                    wordAdapter.notifyDataSetChanged()
+
+                    Toast.makeText(this@MainActivity, "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+                selectedWord = null
+            }
+
+        }.start()
+    }
+
+    private fun editWord() {
+        if (selectedWord == null)
+            return
+
+        val intent = Intent(this, AddActivity::class.java).putExtra("originWord", selectedWord)
+        updateEditWordResult.launch(intent)
+    }
+
+    override fun onCLick(item: Word) {
+        selectedWord = item
+        binding.tvWord.text = item.word
+        binding.tvWordMean.text = item.mean
     }
 }
--- README.md
+++ README.md
@@ -17,8 +17,8 @@
 - [계산기 앱](https://github.com/damon-911/FastCampus/tree/main/Part1/chapter5)
 - [스톱워치 앱](https://github.com/damon-911/FastCampus/tree/main/Part1/chapter6)
 - [단어장 앱](https://github.com/damon-911/FastCampus/tree/main/Part1/chapter7)
-- 나만의 액자 앱
-- 음악재생 앱
+- [나만의 액자 앱](https://github.com/damon-911/FastCampus/tree/main/Part1/chapter8)
+- [음악재생 앱](https://github.com/damon-911/FastCampus/tree/main/Part1/chapter9)
 
 <!--
 ### Part 2. 개발의 퀄리티를 높이는 라이브러리의 활용
