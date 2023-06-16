8b77a50f93f92a2d3af0f5ba9b1d011f7558e816
Minjun Kim <damon911@naver.com>
2023-06-16 21:25:48 +0900

    Part 1 chapter9

---
+++ Part1/chapter9/app/src/main/java/fastcampus/part1/chapter9/Constants.kt
@@ -0,0 +1,6 @@
+package fastcampus.part1.chapter9
+
+const val CHANNEL_ID = "MEDIA_PLAYER_CHANNEL"
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
--- Part1/chapter9/app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
+++ Part1/chapter9/app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
@@ -1,11 +1,45 @@
 package fastcampus.part1.chapter9
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fastcampus.part1.chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivPlay.setOnClickListener { mediaPlay() }
+        binding.ivPause.setOnClickListener { mediaPause() }
+        binding.ivStop.setOnClickListener { mediaStop() }
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, MediaPlayerService::class.java))
+        super.onDestroy()
+    }
+
+    private fun mediaPlay() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+
+    }
+
+    private fun mediaPause() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
+    }
+
+    private fun mediaStop() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
     }
 }
