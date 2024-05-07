e6ec3af495cad71e559a1c1ed525bcb1bc418adb
Minjun Kim <damon911@naver.com>
2024-05-07 12:21:57 +0900

    Part4 chapter3 Row

--- Part4/chapter3/app/src/main/java/fastcampus/part4/chapter3/example/RowExample.kt
+++ Part4/chapter3/app/src/main/java/fastcampus/part4/chapter3/example/RowExample.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
