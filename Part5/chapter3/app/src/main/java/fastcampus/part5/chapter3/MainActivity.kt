1117903b981e2fc0fd179fba70c8cdd3601191ff
Minjun Kim <damon911@naver.com>
2024-06-15 22:24:53 +0900

    Part5 chapter3 Movie App : Navigation 기본 설정

--- Part5/chapter3/app/src/main/java/fastcampus/part5/chapter3/MainActivity.kt
+++ Part5/chapter3/app/src/main/java/fastcampus/part5/chapter3/MainActivity.kt
@@ -1,11 +1,11 @@
 package fastcampus.part5.chapter3
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ Part5/chapter3/app/src/main/java/fastcampus/part5/chapter3/feature/feed/FeedFragment.kt
@@ -0,0 +1,29 @@
+package fastcampus.part5.chapter3.feature.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import fastcampus.part5.chapter3.ui.theme.MovieAppTheme
+
+@AndroidEntryPoint
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MovieAppTheme {
+                    Text(text = "FeedFragment")
+                }
+            }
+        }
+    }
+}
